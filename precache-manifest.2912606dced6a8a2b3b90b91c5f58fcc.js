5492e06ad3f0942b6f88e2d4ea5834a29ee6f303
Gabi B <Gab3009@Gmail.com>
2020-11-19 11:34:59 +0200

    Updates

---
+++ precache-manifest.2912606dced6a8a2b3b90b91c5f58fcc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4220f9adc13fca4c5481dcf34eff77ee",
+    "url": "/path-visualize/index.html"
+  },
+  {
+    "revision": "13197733c1f8b06d10f6",
+    "url": "/path-visualize/static/js/2.75bb79be.chunk.js"
+  },
+  {
+    "revision": "a6b003039f0071b9bc6b063805b1ac89",
+    "url": "/path-visualize/static/js/2.75bb79be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9d2b47f6048e6cb555",
+    "url": "/path-visualize/static/js/main.ae4dbb77.chunk.js"
+  },
+  {
+    "revision": "aa521d839737744f2025",
+    "url": "/path-visualize/static/js/runtime-main.1db36387.js"
+  },
+  {
+    "revision": "a60638237a8eca001cd56ad1e4c63e37",
+    "url": "/path-visualize/static/media/Arimo-Bold.a6063823.ttf"
+  },
+  {
+    "revision": "9fe23bf225eecfcdb736e063b08a6123",
+    "url": "/path-visualize/static/media/Arimo-Regular.9fe23bf2.ttf"
+  },
+  {
+    "revision": "077ceb9111e90dea3fc3923fe71805a1",
+    "url": "/path-visualize/static/media/Muli-Bold.077ceb91.ttf"
+  },
+  {
+    "revision": "328d557958b18b54b3bddb3a4a36215a",
+    "url": "/path-visualize/static/media/Muli-Regular.328d5579.ttf"
+  },
+  {
+    "revision": "1783c49733e18dd222d24d3f50875bf8",
+    "url": "/path-visualize/static/media/end.1783c497.svg"
+  },
+  {
+    "revision": "c10390fd0fc620ad2eb1502fd289a7f6",
+    "url": "/path-visualize/static/media/wall.c10390fd.png"
+  },
+  {
+    "revision": "d2f7167e28133a50bb32eb9af95eee95",
+    "url": "/path-visualize/static/media/weight.d2f7167e.png"
+  }
+]);
